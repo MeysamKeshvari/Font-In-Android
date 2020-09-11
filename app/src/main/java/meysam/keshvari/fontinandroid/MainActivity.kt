50b73843f71dabb4a17b57be26787d46ff5363e6
Meysam Keshvari <meysamkeshvari@gmail.com>
2020-09-11 09:36:38 +0430

    last updated

--- app/src/main/java/meysam/keshvari/fontinandroid/MainActivity.kt
+++ app/src/main/java/meysam/keshvari/fontinandroid/MainActivity.kt
@@ -3,15 +3,21 @@ package meysam.keshvari.fontinandroid
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val typeFace = Typeface.createFromAsset(assets,"calibri.ttf")
-        textView.typeface = typeFace
+        //val typeface = Typeface.createFromAsset(assets,"calibri.ttf")
+        //textView.typeface = typeface
+
+        // or
+        val typeface = ResourcesCompat.getFont(this,R.font.b_titr_bold)
+        textView.typeface = typeface
 
     }
 }
